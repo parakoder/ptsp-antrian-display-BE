bfe58267fe082cf0e0010856a04541125770c734
parakoder <79524587+parakoder@users.noreply.github.com>
2021-09-03 14:58:52 +0700

    Update db.go
    heroku

--- config/db.go
+++ config/db.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"fmt"
+	//"fmt"
 	"log"
 	"os"
 
@@ -21,20 +21,21 @@ func ConnectSQL() (*DB, error){
 	if err != nil {
 		log.Fatal("Error Loading .env file")
 	}
-	dbName := os.Getenv("DB_NAME")
-	password := os.Getenv("PASS")
-	hostName := os.Getenv("HOST")
-	userName := os.Getenv("USER_DB")
-	hostPort := os.Getenv("PORT")
+	//dbName := os.Getenv("DB_NAME")
+	//password := os.Getenv("PASS")
+	//hostName := os.Getenv("HOST")
+	//userName := os.Getenv("USER_DB")
+	//hostPort := os.Getenv("PORT")
 
-	url := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
-		userName,
-		password,
-		hostName,
-		hostPort,
-		dbName)
+	//url := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
+		//userName,
+		//password,
+		//hostName,
+		//hostPort,
+		//dbName)
 		// log.Println("DB COnn ", d)
-		d, err := sqlx.Open("postgres", url)
+		//d, err := sqlx.Open("postgres", url)
+		d, err := sqlx.Open("postgres", os.Getenv("DATABASE_URL"))
 		if err != nil {
 			panic(err)
 		}
